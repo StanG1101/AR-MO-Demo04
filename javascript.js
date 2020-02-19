b699e04dde6113cadec3c0671c37b472e003123a
StanG1101 <omega.dsgb@gmail.com>
2020-02-19 09:02:27 -0500

    AR Pato Anim Propia +  rotar

---
+++ javascript.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('loader', {
+        init: function () {
+                let loader = document.querySelector("#loadingEl")
+                this.el.addEventListener('modelo-loaded', e => {
+                        console.log('loaded')
+                        loader.setAttribute("visible", "false")
+                })
+        }
+});
